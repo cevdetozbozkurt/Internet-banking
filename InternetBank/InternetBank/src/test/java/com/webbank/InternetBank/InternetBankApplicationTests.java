6cdda672d95b20873a4264baa028b780594afb3b
Cevdet Eren Özbozkurt <cevdet.eren.2001@gmail.com>
2023-05-04 00:59:13 +0300

    paketler ve classlar olusturuldu yeni bir spring boot maven projesi dependecy leri ile eklendi

---
+++ InternetBank/InternetBank/src/test/java/com/webbank/InternetBank/InternetBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webbank.InternetBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
